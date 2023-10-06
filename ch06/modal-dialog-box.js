de17b3769a769c54c0444f43983ac70623e9bc5e
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2023-10-06 22:57:14 +0200

    Disable page scrolling

--- ch06/modal-dialog-box.js
+++ ch06/modal-dialog-box.js
@@ -6,10 +6,12 @@ var modal = document.getElementById("modal");
 button.addEventListener("click", function (event) {
   event.preventDefault();
   modal.classList.add("is-open");
+  document.body.classList.add("no-scroll");
 });
 
 // Closes the modal when the user clicks the Close button
 close.addEventListener("click", function (event) {
   event.preventDefault();
   modal.classList.remove("is-open");
+  document.body.classList.remove("no-scroll");
 });
